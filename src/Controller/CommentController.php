212b2ea24dbbf729d6ef66e0560bc4d6666753f4
root <peacerock89@gmail.com>
2022-01-23 22:20:38 +0200

    23/04/2022

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Comment;
 use App\Entity\Post;
 use App\Form\CommentType;
+use App\Form\PostType;
 use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -44,5 +45,34 @@ class CommentController extends AbstractController
         }
 
         return $this->redirectToRoute('post_index', [],Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('comment/{id}/edit', name: 'comment_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PostType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('post_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('post/edit.html.twig', [
+            'post' => $comment,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/comment/{id}/delete', name: 'comment_delete', methods: ['POST'])]
+    public function delete(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $comment->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($comment);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('post_index', [], Response::HTTP_SEE_OTHER);
     }
 }
